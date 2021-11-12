8ba61466de8ec74f110102430416e770bc05741a
Bunny McIntyre <bmcintyre.133@gmail.com>
2021-11-11 19:55:40 -0600

    second commit, not final, moving to studio review

--- scripts.js
+++ scripts.js
@@ -4,21 +4,44 @@ window.addEventListener('load', (event) => {
     
     
     takeoff.addEventListener('click', event => {
-        let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
-        if (response === true) {
+      let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+      if (response === true) {
+        flightStatus.innerHTML = "Shuttle in flight."
+        shuttleBackground.style.backgroundColor = "blue"
+        document.getElementById("spaceShuttleHeight").innerHTML = "10,000"
+      }
+    });
 
-            flightStatus.innerHTML = "Shuttle in flight."
-            shuttleBackground.backgroundColor = "blue"
-            spaceShuttleHeight.innerHMTL = "10,000"
+    landing.addEventListener('click', event => {
+      window.alert("The shuttle is landing. Landing gear engaged.");
+      flightStatus.innerHTML = "The shuttle has landed."
+      shuttleBackground.style.backgroundColor = "green"
+      document.getElementById("spaceShuttleHeight").innerHTML = 0
+    });
 
+    missionAbort.addEventListener('click', event => {
+      let response = window.confirm("Confirm that you want to abort the mission.");
+      if (response === true) {
+        flightStatus.innerHTML = "Mission aborted."
+        shuttleBackground.style.backgroundColor = "green"
+        document.getElementById("spaceShuttleHeight").innerHTML = 0
+      }
+    });
 
+    up.addEventListener('click', event => {
+      rocket.style.position.bottom = "10px"
+    });
 
+    down.addEventListener('click', event => {
+      rocket.style.position.bottom = "10px"
+    });
 
-        });
+    left.addEventListener('click', event => {
+      rocket.style.position.bottom = "10px"
+    });
 
-
-
-
-
+    right.addEventListener('click', event => {
+      rocket.style.position.bottom = "10px"
+    });
 
   });
