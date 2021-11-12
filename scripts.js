c02c8909f4c8825c5af17579500addfa863b3719
Bunny McIntyre <bmcintyre.133@gmail.com>
2021-11-11 18:34:21 -0600

    First commit testing section 2

--- scripts.js
+++ scripts.js
@@ -1,2 +1,24 @@
 // Write your JavaScript code here.
 // Remember to pay attention to page loading!
+window.addEventListener('load', (event) => {
+    
+    
+    takeoff.addEventListener('click', event => {
+        let response = window.confirm("Confirm that the shuttle is ready for takeoff.");
+        if (response === true) {
+
+            flightStatus.innerHTML = "Shuttle in flight."
+            shuttleBackground.backgroundColor = "blue"
+            spaceShuttleHeight.innerHMTL = "10,000"
+
+
+
+
+        });
+
+
+
+
+
+
+  });
